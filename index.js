986b7876327fabb2ca3d554d340b56b7a41866b6
FacuBelli <facundo_carabelli@hotmail.com>
2023-06-06 15:33:58 -0300

    Agregado de validacion en login

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ $(document).ready(function () {
     })
 
 })
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var usernameInput = document.getElementById('exampleInputEmail1');
+    var passwordInput = document.getElementById('exampleInputPassword1');
+    var loginButton = document.getElementById('ingresar');
+    var usernameError = document.getElementById('usernameError');
+    var passwordError = document.getElementById('passwordError');
+  
+    function validateForm() {
+      var username = usernameInput.value;
+      var password = passwordInput.value;
+      var isUsernameValid = username.length >= 8;
+      var isPasswordValid = password.length >= 8;
+  
+      usernameError.textContent = isUsernameValid ? '' : 'El nombre de usuario debe tener al menos 8 caracteres';
+      passwordError.textContent = isPasswordValid ? '' : 'La contraseña debe tener al menos 8 caracteres';
+  
+      loginButton.disabled = !(isUsernameValid && isPasswordValid);
+    }
+  
+    usernameInput.addEventListener('input', validateForm);
+    passwordInput.addEventListener('input', validateForm);
+  });
+  
+  
